18731ce168d7d1e6567baac94225ccdfc88109b1
Michaela Wells <mgwells012@gmail.com>
2022-10-09 18:22:21 -0400

    added in routes folder to keep script cleaner, wrote out html route

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const fs = require ('fs');
 const PORT = process.env.PORT || 3001;
 const app = express ();
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get
+app.get("/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), //"utf8",
+     (error, notes) => {
+        if (error) {
+            return console.log("Note Cannot Be Found")
+        }
+        res.json(JSON.parse(notes))
+    })
+});
 
 
 app.post
