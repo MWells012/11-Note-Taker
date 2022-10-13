b13ae8a74307d253f9b8ea3bbd6f0549b976e96b
Michaela Wells <mgwells012@gmail.com>
2022-10-12 21:52:42 -0400

    push all work done, API Route page not working correctly

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,10 +1,46 @@
 const router = require("express").Router();
-const db = require("../db/db.json")
+const path = require("path");
+const db = require("../db/db.json");
+const fs = require ("fs");
 console.log(db);
 
 router.get("/api/notes", (req, res) => {
-    res.send(db)
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (error, notes) => {
+      if (error) {
+        return console.log("error: could not get note")
+      }
+      res.json(JSON.parse(notes))
+    })
 });
+
+
+router.post("/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("../db/db.json"));
+  const newNotes = req.body;
+  if (notes.length === 0) {
+    newNotes.id = 1
+  } else {
+    newNotes.id = notes.length + 1;
+  }
+  notes.push(newNotes);
+  fs.writeFileSync("../db/db.json", JSON.stringify(notes), (err) => {
+    if(err) {
+      console.log("error: could not add note")
+    }
+  })
+  res.json(notes);
+  console.log("new note has been added")
+});
+
+router.delete("/notes/:id", (req, res) => {
+
+  fs.readFile("..db/db.json", (err, data) => {
+    if (err) throw err;
+
+    noteData = JSON.parse(data)
+  })
+});
+
 
 
 
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const { application, Router } = require("express");
-
-Router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
